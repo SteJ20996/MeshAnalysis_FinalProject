26549e38e6d7640adca29b4e9e802f5c308cfb60
Yizhen Jia <stephenjia3852@gmail.com>
2022-12-06 11:54:42 -0500

    update on mainp.cpp and func.cpp

--- src/manip.cpp
+++ src/manip.cpp
@@ -1,9 +1,5 @@
 #include "manip.hpp"
-#include <algorithm>
-#include <array>
 #include <cmath>
-#include <iostream>
-#include <vector>
 // you will need cmath and algorithm headers.
 
 namespace ams562_final {
@@ -88,13 +84,3 @@ void compute_errors(const SphCo &exact, const SphCo &num,
 }
 
 }  // namespace ams562_final
-
-int main(){
-    int n = 10;
-    std::vector< std::vector<int> > x(n);
-    std::cout << x.size() << std::endl;
-    for(int idx = 0; idx < n; idx++){
-        x[idx].push_back(idx);
-        std::cout << x[idx].size() << std::endl;
-    }
-}
